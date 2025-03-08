5a53dbc0a0e20d71629b5f6859d821e29f1234ba
naimish73 <naimishbbhuva@gmail.com>
2025-03-08 12:12:10 +0530

    ci: made basic ui design structure in home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import "./styles.css"
 
+// assets import
+import page from "./../../assets/one.png"
+
 const Home: React.FC = () => {
     return (
         <>
-            <div className="home text-3xl font-bold underline">
-                hello world!!
+            <div className="home">
+                <div className="flex flex-col">
+                    <div className="flex flex-col lg:flex-row">
+                        <div className="w-full lg:w-[60%]">
+                            <img src={page} alt="paper page" />
+                        </div>
+                        <div className="flex lg:flex-col w-full lg:w-[10%]">
+                            <div className="w-full">Tool box</div>
+                            <div className="w-full">Marks</div>
+                        </div>
+                        <div className="w-full lg:w-[30%]">Question</div>
+                    </div>
+
+
+                    <div>Page Utility</div>
+                </div>
             </div>
         </>
     )
